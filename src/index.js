df24eda155649ccb82b0a9d5b8c99ceb4736a9a7
Sue Ceklosky <cekloskys@chc.edu>
2022-06-20 09:01:29 -0400

    Heroku changes

--- src/index.js
+++ src/index.js
@@ -383,7 +383,11 @@ const start = async () => {
     });
 
     // The `listen` method launches a web server.
-    server.listen().then(({ url }) => {
+    /* server.listen().then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    }); */
+
+    server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
         console.log(`🚀  Server ready at ${url}`);
     });
 }
